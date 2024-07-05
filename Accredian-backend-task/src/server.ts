8d9a2ed8f06393becb729f96a60291afc0f04288
shubhsardana29 <shubhsardana29@gmail.com>
2024-07-05 20:34:44 +0530

    chore: Update CORS middleware configuration for server.js

--- Accredian-backend-task/src/server.ts
+++ Accredian-backend-task/src/server.ts
@@ -1,7 +1,11 @@
 import app from "./app";
 import cors from 'cors';
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const PORT = process.env.PORT || 3000;
 
